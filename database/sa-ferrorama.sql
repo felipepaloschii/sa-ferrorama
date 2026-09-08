b17349e3793693ba4856852eae20fbc092637cc6
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-09-08 10:06:46 -0300

    add - sql em andamento

--- database/sa-ferrorama.sql
+++ database/sa-ferrorama.sql
@@ -1,4 +1,15 @@
-create database sa_ferrorama_fjl
-use sa_ferrorama_fjl;
+create database sa_ferrorama_db;
+use sa_ferrorama_db;
 
-create table 
+create table usuario (
+    id_usuario int primary key auto_increment,
+    email varchar(100) not null unique,
+    senha varchar(100) not null
+)
+
+create table sensor (
+
+    id int auto_increment primary key,
+    
+)
+
