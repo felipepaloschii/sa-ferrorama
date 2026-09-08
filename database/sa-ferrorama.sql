93db05aba4a85a584dfdcedae746bc7197eda6b4
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-09-08 11:00:03 -0300

    add - sql terminado

--- database/sa-ferrorama.sql
+++ database/sa-ferrorama.sql
@@ -5,11 +5,18 @@ create table usuario (
     id_usuario int primary key auto_increment,
     email varchar(100) not null unique,
     senha varchar(100) not null
-)
+);
 
 create table sensor (
 
-    id int auto_increment primary key,
-    
-)
+    id int primary key auto_increment,  
+    nome_sensor varchar (100)  not null,
+    tipo_sensor varchar (100) not null,
+    localizacao varchar (150) not null,
+    unidade_medida varchar (20) not null,
+    limite_alerta decimal (10,2) not null,
+    status_inicial text, 
+    descricao text
+
+);
 
