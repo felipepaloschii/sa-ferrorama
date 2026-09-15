2b9a076b4cce1564cbe6ff2cfaaddcb379a4fdcf
Lucas <lucas_af_bezerra@estudante.sesisenai.org.br>
2026-09-15 11:48:22 -0300

    Upd - Nome Projeto

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - FJL Tech</title>
+    <title>Home - PIA Enterprise</title>
     <link rel="stylesheet" href="assets/style/style.css">
 </head>
 <body id="home-corpo">
     <header class="barra-superior">
-        <h1>FJL Tech</h1>
+        <h1>PIA Enterprise</h1>
 
         <div class="acoes-superiores">
             <span class="ponto-online"></span>
