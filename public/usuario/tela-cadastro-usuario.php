9c2087be897d988a72cdd9ee544fd967a8c6fac3
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-09-24 11:14:45 -0300

    add - sql rotas

--- public/usuario/tela-cadastro-usuario.php
+++ public/usuario/tela-cadastro-usuario.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro - FJL Tech</title>
+    <title>Cadastro - PIA Enterprise</title>
     <link rel="stylesheet" href="../../assets/style/style.css">
 </head>
 
--- public/usuario/tela-login.php
+++ public/usuario/tela-login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"]  == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - FJL Tech</title>
+    <title>Login - PIA Enterprise</title>
     <link rel="stylesheet" href="../../assets/style/style.css">
 </head>
 
